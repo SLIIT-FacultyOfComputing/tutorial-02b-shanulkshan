e89fa31e7a2f8a4675c3f47360ba39b83eae35ea
shanuka lakshan <99382245+shanulkshan@users.noreply.github.com>
2022-03-06 12:29:23 +0000

    completed

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,14 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no){
+  int fac=1;
+  for(int r=no;r>=1;r--){
+    fac=fac*r;
+  }
+  return fac;
+}
+long nCr(int n,int r){
+  int x=Factorial(n)/(Factorial(r)*Factorial(n-r));
+  return x;
+}
